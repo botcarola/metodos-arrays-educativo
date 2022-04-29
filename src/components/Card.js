0d6fd44e9dfa8067d82ee733746be4582326e255
Elizabeth Carolina <celerner4@gmail.com>
2022-04-29 17:46:24 -0300

    desarrollo componente Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,18 @@
 import "../styles/_Card.scss";
+import { Link } from "react-router-dom";
+import { BsArrowRightShort } from "react-icons/bs";
 
-const Card = () => {
+const Card = ({metodo}) => {
     return (
-        <div class="card">
+        <Link to={`/metodo/${metodo}`} class="card">
+            <div className="card-title">
+                <h2>
+                    {metodo}                
+                </h2>
+                <BsArrowRightShort />
+            </div>
             
-        </div>
+        </Link>
     )
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,16 @@
 import Card from "./Card";
+import "../styles/_Home.scss"
 
 const Home = () => {
     return (
-        <>
-            <Card />       
-        </>
+        <main>
+            <h1>
+                Métodos de <a href="https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array">arrays</a> en JavaScript
+            </h1>
+            <Card 
+            metodo={"push"}
+            />       
+        </main>
     )
 }
 
